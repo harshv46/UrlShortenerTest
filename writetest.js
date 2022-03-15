80dd87fe02cfdc2944847e549c48cb6800324d2a
harshv46 <harshvardhan21364@gmail.com>
2022-03-16 01:58:49 +0530

    updated files

--- writetest.js
+++ writetest.js
@@ -271,26 +271,26 @@ function randomExponential(rate) {
 
 export const options = {
 	duration: '60m',
-	vus: 10,
+	vus: 1,
 	maxRedirects: 0,
 };
-export default function () {
-	const requests = [];
-	for(let i=0;i<80;i++){
-		const req1 = {
-			method: 'GET',
-			url: `https://url-shortener4-dev.ap-south-1.elasticbeanstalk.com/${getPareto(1001)-1}_${Math.floor(Math.random()*999)}`,
-		};
-		requests.push(req1);
-	}
-	const res = http.batch(requests);
-	sleep(randomExponential(1));
+// export default function () {
+// 	const requests = [];
+// 	for(let i=0;i<80;i++){
+// 		const req1 = {
+// 			method: 'GET',
+// 			url: `https://url-shortener4-dev.ap-south-1.elasticbeanstalk.com/${getPareto(1001)-1}_${Math.floor(Math.random()*999)}`,
+// 		};
+// 		requests.push(req1);
+// 	}
+// 	const res = http.batch(requests);
+// 	sleep(randomExponential(1));
 	
-	// const res = http.get(`http://url-shortener4-dev.ap-south-1.elasticbeanstalk.com/${getPareto(1001)-1}_${Math.floor(Math.random()*999)}`);
-	// console.log(res.body);
-	// if(res.timings.duration !==0 )
-		// console.log(Date.now(), res.timings.duration);
-}
+// 	// const res = http.get(`http://url-shortener4-dev.ap-south-1.elasticbeanstalk.com/${getPareto(1001)-1}_${Math.floor(Math.random()*999)}`);
+// 	// console.log(res.body);
+// 	// if(res.timings.duration !==0 )
+// 		// console.log(Date.now(), res.timings.duration);
+// }
 
 
 
@@ -314,36 +314,37 @@ export default function () {
 // discrete.js
 // Sample from discrete distributions.
 
-// export default function ()
-// {
-//     // var short = getRandomString(getRandomInt(1));
-//     // var lurl = "https://www.google.com/search?q=";
-//     // console.log(short);
-//     // const pre = "https://github.com/shubham11941140/short_url/";
-//     const url = 'http://url-shortener4-dev.ap-south-1.elasticbeanstalk.com/';
-//     const payload = JSON.stringify({
-//         // urldata: pre + short,
-//         // shorturl: `${getPareto(150)}_${Math.floor(Math.random()*999)}`
-//         long_url: "https://www.google.com",
-//          short_url: `test${getPareto(150)}_${Math.floor(Math.random()*999)}`,
-//         // short_url: ,
-//           exp_date: 31
-//     });
-//     // curl    -H "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJhbnkiLCJleHAiOjE2NDk2NTg0NjYsImlzcyI6ImF1dGgtYXBwIiwic3ViIjoiY3M1NTkifQ._JshleXML9zqsV4sDAtaoBhxKPldyE2MPw_2Fo8XjGw"
+export default function ()
+{
+    // var short = getRandomString(getRandomInt(1));
+    // var lurl = "https://www.google.com/search?q=";
+    // console.log(short);
+    // const pre = "https://github.com/shubham11941140/short_url/";
+    const url = 'https://url-shortener4-dev.ap-south-1.elasticbeanstalk.com/';
+    const payload = JSON.stringify({
+        // urldata: pre + short,
+        // shorturl: `${getPareto(150)}_${Math.floor(Math.random()*999)}`
+        long_url: "https://www.google.com",
+        short_url: `wrt${getPareto(150)}_${Math.floor(Math.random()*999)}`,
+        // short_url: ,
+          exp_date: 31
+    });
+    // curl    -H "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJhbnkiLCJleHAiOjE2NDk2NTg0NjYsImlzcyI6ImF1dGgtYXBwIiwic3ViIjoiY3M1NTkifQ._JshleXML9zqsV4sDAtaoBhxKPldyE2MPw_2Fo8XjGw"
 
-//         console.log(payload);
-//     const params = {
-//     headers: {
-//         'Content-Type': 'application/json',
-//         'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJhbnkiLCJleHAiOjE2NDk2NTg0NjYsImlzcyI6ImF1dGgtYXBwIiwic3ViIjoiY3M1NTkifQ._JshleXML9zqsV4sDAtaoBhxKPldyE2MPw_2Fo8XjGw'
-//     },
-//     };
+        // console.log(payload);
+    const params = {
+    headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJhbnkiLCJleHAiOjE2NDk2NTg0NjYsImlzcyI6ImF1dGgtYXBwIiwic3ViIjoiY3M1NTkifQ._JshleXML9zqsV4sDAtaoBhxKPldyE2MPw_2Fo8XjGw'
+    },
+    };
 
-//     // send a HTTP POST request
-//     // console.log(payload);
-//     const res = http.post(url, payload, params);
-//     // console.log(res.body);
-// }
+    // send a HTTP POST request
+    // console.log(payload);
+    const res = http.post(url, payload, params);
+    sleep(randomExponential(50));
+    // console.log(res.body);
+}
 
 
 // export default function ()
